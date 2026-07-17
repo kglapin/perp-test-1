756bffd979fcffc8603cde0e3f8ac3c1657627c6
kglapin <kamilglapinski.poczta@gmail.com>
2026-07-17 12:55:18 +0200

    feat: build premium storytelling tiling website

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://kglapin.github.io',
-  base: '/perp-test-1',
-});
